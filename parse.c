230a1c4f4725d3295395e7c02b0bdea22f5bfce4
satish <seerpini@purdue.edu>
2011-12-21 16:54:47 -0500

    Some more parsing code, execvp with command works

--- parse.c
+++ parse.c
@@ -45,8 +45,6 @@ int * break_by_spaces(char * arg, int * num_args){
 }
 
 int main(int argc, char *argv[]){
-
-
         /*
          * Core parsing logic, look for `` in the arguments
          */
@@ -55,24 +53,43 @@ int main(int argc, char *argv[]){
                 return EXIT_FAILURE;
         }
 
+        pid_t child = 0, tpid = 0;
         int i= 0;
         int count = 0;
         int * prog_exec_ind = break_by_spaces(argv[1], &count);
+        int child_status = 0;
         fprintf(stdout, "The number of args is : [%d]\n", count);
-        char *args_for_exec[count];
+        char *args_for_exec[count+1];
 
         if(prog_exec_ind == NULL){
                 fprintf(stderr, "Cannot exec program specified as first arg\n");
                 return EXIT_FAILURE;
         }
 
+        //We can also use i < count for the condition in the for block
         for(i=0; *(prog_exec_ind + i) != -1; i++){
                 fprintf(stdout, "[%d] %d %s \n", i, *(prog_exec_ind + i), (char *)(argv[1] + *(prog_exec_ind + i)));
-
+                *(args_for_exec+ i) = strdup((char *)(argv[1] + *(prog_exec_ind + i))); 
+        }
+        *(args_for_exec + count) = NULL;
+        for(i=0; i< count-1; i++){
+                fprintf(stdout, "[%d] %s\n", i, *(args_for_exec+i));
         }
 
-
-
+        child = fork();
+        if(child == 0){
+               if( execvp((char *)args_for_exec[0], (char **)args_for_exec) == -1){
+                       perror("execvp");
+                       exit(EXIT_FAILURE);
+               }
+        }
+        /*
+         * Wait for the child to finish in the parent
+         */
+        else{
+                do{
+                        tpid = wait(&child_status);
+                }while(tpid != child);
+        }
         return EXIT_SUCCESS;
-
 }
